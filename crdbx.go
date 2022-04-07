972d0dc6028aa79ae4f1f62657d45eaa190af1f9
David López <d.lopez@saltosystems.com>
2022-04-07 12:46:59 +0200

    implement begintx

--- crdbx.go
+++ crdbx.go
@@ -19,6 +19,10 @@ type Conn struct {
 	pgxConn pgx.Conn
 }
 
+func (c *Conn) Begin() (driver.Tx, error) {
+	return c.pgxConn.Begin()
+}
+
 func (c *Conn) Prepare(query string) (driver.Stmt, error) {
 	return c.pgxConn.Prepare(query)
 }
@@ -27,8 +31,8 @@ func (c *Conn) Close() error {
 	return c.pgxConn.Close()
 }
 
-func (c *Conn) Begin() (driver.Tx, error) {
-	return c.pgxConn.Begin()
+func (c *Conn) BeginTx(ctx context.Context, opts driver.TxOptions) (driver.Tx, error) {
+	return c.pgxConn.BeginTx(ctx, opts)
 }
 
 // TODO(d.lopez) consider implementing Connector and DriverContext interfaces, so we can make use of OpenDB method
@@ -73,4 +77,18 @@ func (c *Conn) QueryContext(ctx context.Context, query string, argsV []driver.Na
 		return nil, err
 	}
 	return rows, nil
+}
+
+func (c pgxTxAdapter) Commit(ctx context.Context) error {
+	return tx.tx.Commit(ctx)
+}
+
+func (tx pgxTxAdapter) Rollback(ctx context.Context) error {
+	return tx.tx.Rollback(ctx)
+}
+
+// Exec is part of the crdb.Tx interface.
+func (tx pgxTxAdapter) Exec(ctx context.Context, q string, args ...interface{}) error {
+	_, err := tx.tx.Exec(ctx, q, args...)
+	return err
 }
