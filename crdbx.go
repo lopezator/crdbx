626c21711d7f76adc62a0c4bae67de60f08ae839
David Lobe <not4rent@gmail.com>
2023-11-21 15:36:49 +0100

    *: update to latest goland, pgx and simplify
    Update to latest versions and simplify the code.

--- crdbx.go
+++ crdbx.go
@@ -4,61 +4,51 @@ import (
 	"context"
 	"database/sql"
 	"database/sql/driver"
+	"sync"
 
 	"github.com/cockroachdb/cockroach-go/v2/crdb"
-	pgx "github.com/jackc/pgx/v4/stdlib" // force psql driver import
 )
 
-// register crdbx driver on library import
-func init() {
-	sql.Register("crdbx", &Driver{pgxDriver: &pgx.Driver{}})
-}
-
-// Conn implement database/sql/driver.Conn interface
-type Conn struct {
-	pgxConn pgx.Conn
-}
-
-func (c *Conn) Begin() (driver.Tx, error) {
-	return c.pgxConn.Begin()
-}
-
-func (c *Conn) Prepare(query string) (driver.Stmt, error) {
-	return c.pgxConn.Prepare(query)
-}
-
-func (c *Conn) Close() error {
-	return c.pgxConn.Close()
-}
-
-func (c *Conn) BeginTx(ctx context.Context, opts driver.TxOptions) (driver.Tx, error) {
-	return c.pgxConn.BeginTx(ctx, opts)
-}
-
-// TODO(d.lopez) consider implementing Connector and DriverContext interfaces, so we can make use of OpenDB method
-//   instead of
-//   encoding all configuration into an string passed to sql.Open, more details here:
-//   https://golang.org/doc/go1.10#database/sql/driver
-
-// Driver implement database/sql/driver.Driver interface
+// Driver implements database/sql/driver.Driver interface
 type Driver struct {
-	pgxDriver *pgx.Driver
+	driver.Driver
 }
 
-func (d *Driver) Open(name string) (driver.Conn, error) {
-	conn, err := d.pgxDriver.Open(name)
+// conn implements database/sql/driver.Driver interface
+type conn struct {
+	driver.Conn
+}
+
+// Mutex to protect driver registration.
+var m sync.Mutex
+
+// Register registers the driver.
+func Register() error {
+	// Retrieve the driver implementation we need to wrap.
+	db, err := sql.Open("pgx", "")
 	if err != nil {
-		return nil, err
+		return err
 	}
-	return &Conn{pgxConn: *conn.(*pgx.Conn)}, nil
+	drv := db.Driver()
+	if err = db.Close(); err != nil {
+		return err
+	}
+
+	// Register the driver name.
+	m.Lock()
+	defer m.Unlock()
+	sql.Register("crdx", drv)
+
+	// Return.
+	return nil
 }
 
-// ExecContext implement database/sql/driver.ExecerContext interface
-func (c *Conn) ExecContext(ctx context.Context, query string, argsV []driver.NamedValue) (driver.Result, error) {
+// ExecContext implement database/sql/driver.ExecerContext interface.
+func (c *conn) ExecContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Result, error) {
 	var result driver.Result
 	if err := crdb.Execute(func() error {
 		var err error
-		result, err = c.pgxConn.ExecContext(crdb.WithMaxRetries(ctx, 5), query, argsV)
+		result, err = c.Conn.(driver.ExecerContext).ExecContext(crdb.WithMaxRetries(ctx, 5), query, args)
 		return err
 	}); err != nil {
 		return nil, err
@@ -67,28 +57,14 @@ func (c *Conn) ExecContext(ctx context.Context, query string, argsV []driver.Nam
 }
 
 // QueryContext implement database/sql/driver.QueryerContext interface
-func (c *Conn) QueryContext(ctx context.Context, query string, argsV []driver.NamedValue) (driver.Rows, error) {
+func (c *conn) QueryContext(ctx context.Context, query string, args []driver.NamedValue) (driver.Rows, error) {
 	var rows driver.Rows
 	if err := crdb.Execute(func() error {
 		var err error
-		rows, err = c.pgxConn.QueryContext(crdb.WithMaxRetries(ctx, 5), query, argsV)
+		rows, err = c.Conn.(driver.QueryerContext).QueryContext(crdb.WithMaxRetries(ctx, 5), query, args)
 		return err
 	}); err != nil {
 		return nil, err
 	}
 	return rows, nil
-}
-
-func (c pgxTxAdapter) Commit(ctx context.Context) error {
-	return tx.tx.Commit(ctx)
-}
-
-func (tx pgxTxAdapter) Rollback(ctx context.Context) error {
-	return tx.tx.Rollback(ctx)
-}
-
-// Exec is part of the crdb.Tx interface.
-func (tx pgxTxAdapter) Exec(ctx context.Context, q string, args ...interface{}) error {
-	_, err := tx.tx.Exec(ctx, q, args...)
-	return err
 }
--- go.mod
+++ go.mod
@@ -1,21 +1,10 @@
 module github.com/lopezator/crdbx
 
-go 1.17
+go 1.21
+
+require github.com/cockroachdb/cockroach-go/v2 v2.2.8
 
 require (
-	github.com/cockroachdb/cockroach-go/v2 v2.2.8
-	github.com/jackc/pgx/v4 v4.15.1-0.20220305144102-3ce50c079e87
-)
-
-require (
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.11.0 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.2.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
-	github.com/jackc/pgtype v1.10.0 // indirect
 	github.com/lib/pq v1.10.2 // indirect
-	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
